ffa847356cf5b8e4c68699d018a5d1f25e722b36
Aditya chandel <adityachandel371@gmail.com>
2025-02-28 19:24:27 +0530

    Hide and show password

---
+++ Hide password/script.js
@@ -0,0 +1,16 @@
+// js logic
+
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = function () {
+    if (password.type === "password") {
+        password.type = "text";
+        eyeicon.classList.remove("fa-eye-slash");
+        eyeicon.classList.add("fa-eye");
+    } else {
+        password.type = "password";
+        eyeicon.classList.remove("fa-eye");
+        eyeicon.classList.add("fa-eye-slash");
+    }
+};
