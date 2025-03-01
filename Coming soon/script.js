3e7ae0c2dfcd24e3bdb491ea990f247d6bd431ee
Aditya chandel <adityachandel371@gmail.com>
2025-03-01 20:02:30 +0530

    Coming soon page

---
+++ Coming soon/script.js
@@ -0,0 +1,31 @@
+// js logic
+
+var countDownDate = new Date("March 7, 2025 00:00:00").getTime(); // countdown end date
+
+var x = setInterval(function () {
+
+    var now = new Date().getTime(); // current date
+    var distance = countDownDate - now; // countdown time
+
+    // calculate countdown time
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // showing s=changes in the page
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) { // if countdown ends
+        clearInterval(x);
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+
+}, 1000);
