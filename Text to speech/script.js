729dd68ccfb07bcd0733dde20cead7f632cd30e5
Aditya chandel <adityachandel371@gmail.com>
2025-03-06 22:32:44 +0530

    Text to speech convertor

---
+++ Text to speech/script.js
@@ -0,0 +1,30 @@
+// js logic
+
+// getting speech using js
+let speech = new SpeechSynthesisUtterance();
+
+// voices array
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+// changing voices
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    // default voice index 0
+    speech.voice = voices[0];
+
+    // creating all options
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+// changing voices
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+})
+
+// Button for playing the speech
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
